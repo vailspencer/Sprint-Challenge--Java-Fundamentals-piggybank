be8ff885e5bc108c5ebd5cd85f2509390a836dfe
Spencer_Vail <vail.spencer@gmail.com>
2020-08-07 08:24:15 -0700

    initial commit

---
+++ src/main/java/lambdaschool/javapiggybank/JavapiggybankApplication.java
@@ -0,0 +1,16 @@
+package lambdaschool.javapiggybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapiggybankApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavapiggybankApplication.class,
+            args);
+    }
+
+}
